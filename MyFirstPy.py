fda1f8235448e916bae76c2710751bc8360a5bef
satishkumarms <satishkumarms19@gmail.com>
2018-02-08 09:33:37 +0530

    added new file

---
+++ MyFirstPy.py
@@ -0,0 +1,24 @@
+name = input("enter name :");
+
+print("hey,"  + name);
+print('hey'[2]);
+print(name[2]);
+print(name[-4]);
+input();
+/*>>> exam=[1,2,3,4,5,6.7]
+>>> exam[3:4]
+[4]
+>>> exam
+[1, 2, 3, 4, 5, 6.7]
+>>> exam[3:]
+[4, 5, 6.7]
+>>> exam[3:5]
+[4, 5]
+>>> exam[3:6]
+[4, 5, 6.7]
+>>> exam[2:]
+[3, 4, 5, 6.7]
+>>> exam
+[1, 2, 3, 4, 5, 6.7]
+>>> exam[:4]
+[1, 2, 3, 4] 
